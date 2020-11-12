fddb2169c47ebb9490aff9592fecab11ed56f463
kelseybathurst <kelsey.bathurst@gmail.com>
2020-11-12 13:22:58 -0500

    added initial files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Tower of Babel');
+INSERT INTO burgers (burger_name) VALUES ('Judgement Day');
+INSERT INTO burgers (burger_name) VALUES ('Armageddon');
+INSERT INTO burgers (burger_name) VALUES ('The Four Horsemen of the Apocalypse');
